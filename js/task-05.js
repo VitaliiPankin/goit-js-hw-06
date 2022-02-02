e027573e43181eac41ceaa09de89215148a98155
VitaliiPankin <niknap777@ukr.net>
2022-02-02 21:44:42 +0200

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,17 @@
+const inputRef = document.querySelector('#name-input')
+const valueRef = document.querySelector('#name-output')
+console.dir(inputRef)
+
+// inputRef.addEventListener("input", (event) => {
+//     if (event.currentTarget.value === '') {
+//       return  valueRef.textContent = "Anonymous"
+//     }
+//     return valueRef.textContent = event.currentTarget.value
+// })
+
+
+inputRef.addEventListener("input", (event) => {
+    event.currentTarget.value === '' ?
+        valueRef.textContent = "Anonymous" :
+        valueRef.textContent = event.currentTarget.value
+})
