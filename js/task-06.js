f4b7794a23b2e7a19a44543b4aa33de3481f9afa
VitaliiPankin <niknap777@ukr.net>
2022-02-02 22:42:04 +0200

    task-6

---
+++ js/task-06.js
@@ -0,0 +1,21 @@
+const inputRef = document.querySelector("#validation-input")
+
+inputRef.addEventListener("blur", (event) => {
+   
+    const dataSet = Number(event.currentTarget.dataset.length)
+    const valueLength = event.currentTarget.value.length
+    console.dir(dataSet)
+    console.dir(valueLength)
+    if (dataSet === valueLength) {
+        console.log("yes")
+        inputRef.classList.remove("invalid")
+        inputRef.classList.add("valid")
+    } else {
+        console.log("no")
+        inputRef.classList.remove("valid")
+        inputRef.classList.add("invalid")
+    }
+    
+})
+
+
